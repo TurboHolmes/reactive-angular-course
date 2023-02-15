5085dbcf1ca829c4f1ccf70ce61e8be7a7b95b11
TurboHolmes <54500894+TurboHolmes@users.noreply.github.com>
2023-02-15 05:44:45 +0000

    Pending changes exported from your codespace

---
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from "@angular/core";
+import { Course } from "../model/course";
+
+@Component({
+  selector: "courses-card-list",
+  templateUrl: "./courses-card-list.component.html",
+  styleUrls: ["./courses-card-list.component.scss"],
+})
+export class CoursesCardListComponent {
+  @Input() courses: Course[];
+}
