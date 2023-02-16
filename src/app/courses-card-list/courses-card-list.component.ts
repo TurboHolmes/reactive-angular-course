f9eaacf3b71ab14996c9e42cd50b82a7b09b7c76
TurboHolmes <54500894+TurboHolmes@users.noreply.github.com>
2023-02-16 07:48:32 +0000

    Section 2 - 10

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { filter, tap } from "rxjs/operators";
 import { CourseDialogComponent } from "../course-dialog/course-dialog.component";
 import { Course } from "../model/course";
 
@@ -11,6 +12,7 @@ import { Course } from "../model/course";
 export class CoursesCardListComponent {
   @Input() courses: Course[] = [];
 
+  @Output() coursesChanged = new EventEmitter();
   constructor(private dialog: MatDialog) {}
 
   editCourse(course: Course) {
@@ -23,5 +25,13 @@ export class CoursesCardListComponent {
     dialogConfig.data = course;
 
     const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+
+    dialogRef
+      .afterClosed()
+      .pipe(
+        filter((val) => !!val),
+        tap(() => this.coursesChanged.emit(null))
+      )
+      .subscribe();
   }
 }
