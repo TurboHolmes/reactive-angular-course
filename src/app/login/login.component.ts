41c6dbece625e11513f0a1d8947a72b3f8416507
turbo <turbo@noreply.com>
2023-02-19 15:54:07 +0530

    Section 4

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 import { Router } from "@angular/router";
+import { AuthStore } from "../services/auth.store";
 
 @Component({
   selector: "login",
@@ -11,7 +12,11 @@ import { Router } from "@angular/router";
 export class LoginComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private auth: AuthStore
+  ) {
     this.form = fb.group({
       email: ["test@angular-university.io", [Validators.required]],
       password: ["test", [Validators.required]],
@@ -22,5 +27,14 @@ export class LoginComponent implements OnInit {
 
   login() {
     const val = this.form.value;
+
+    this.auth.login(val.email, val.password).subscribe({
+      next: () => {
+        this.router.navigateByUrl("/courses");
+      },
+      error: (err) => {
+        alert("Login failed");
+      },
+    });
   }
 }
